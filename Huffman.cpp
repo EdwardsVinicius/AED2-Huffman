ad78876a0ad72bd8a449964ded6b28e14b2907f3
nicsFantasy <vinicius.edwards@gmail.com>
2019-11-24 21:37:34 -0400

    Arvore com o codigo do geeks for geeks

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,5 +1,4 @@
-#include<iostream>
-#include<string.h>
+#include <bits/stdc++.h>
 #include<vector>
 #include<fstream>
 
@@ -18,15 +17,97 @@ void print(T arr, int tam);
 
 void sortByProb(vector<float> &xa, vector<char> &alph, int tam);
 
-float getMenorProbabilidade(vector<float> &prob);
+//float getMenorProbabilidade(vector<float> &prob);
 
-typedef struct nodeArvore
-{
-    float freq;
-    char c;
-    struct nodeArvore *esquerda;
-    struct nodeArvore *direita;
-} nodeArvore;
+struct MinHeapNode { 
+  
+    // One of the input characters 
+    char data; 
+  
+    // Frequency of the character 
+    unsigned freq; 
+  
+    // Left and right child 
+    MinHeapNode *left, *right; 
+  
+    MinHeapNode(char data, unsigned freq) 
+  
+    { 
+  
+        left = right = NULL; 
+        this->data = data; 
+        this->freq = freq; 
+    } 
+}; 
+  
+// For comparison of 
+// two heap nodes (needed in min heap) 
+struct compare { 
+  
+    bool operator()(MinHeapNode* l, MinHeapNode* r) 
+  
+    { 
+        return (l->freq > r->freq); 
+    } 
+}; 
+  
+// Prints huffman codes from 
+// the root of Huffman Tree. 
+void printCodes(struct MinHeapNode* root, string str) 
+{ 
+  
+    if (!root) 
+        return; 
+  
+    if (root->data != '$') 
+        cout << root->data << ": " << str << "\n"; 
+  
+    printCodes(root->left, str + "0"); 
+    printCodes(root->right, str + "1"); 
+} 
+  
+// The main function that builds a Huffman Tree and 
+// print codes by traversing the built Huffman Tree 
+void HuffmanCodes(vector<char> data, int freq[], int size) 
+{ 
+    struct MinHeapNode *left, *right, *top; 
+  
+    // Create a min heap & inserts all characters of data[] 
+    priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap; 
+  
+    for (int i = 0; i < size; ++i) 
+        minHeap.push(new MinHeapNode(data[i], freq[i])); 
+  
+    // Iterate while size of heap doesn't become 1 
+    while (minHeap.size() != 1) { 
+  
+        // Extract the two minimum 
+        // freq items from min heap 
+        left = minHeap.top(); 
+        minHeap.pop(); 
+  
+        right = minHeap.top(); 
+        minHeap.pop(); 
+  
+        // Create a new internal node with 
+        // frequency equal to the sum of the 
+        // two nodes frequencies. Make the 
+        // two extracted node as left and right children 
+        // of this new node. Add this node 
+        // to the min heap '$' is a special value 
+        // for internal nodes, not used 
+        top = new MinHeapNode('$', left->freq + right->freq); 
+  
+        top->left = left; 
+        top->right = right; 
+  
+        minHeap.push(top); 
+    } 
+  
+    // Print Huffman codes using 
+    // the Huffman tree built above 
+    printCodes(minHeap.top(), ""); 
+} 
 
 
 int main(){
@@ -62,29 +143,24 @@ int main(){
 
     int tam = alphabet.size();
     vector<float> prob(tam);
-    int numOcurrences[tam];
+    int freq[tam];
 
     for (int i = 0; i < tam; i++) {
-        numOcurrences[i] = qtdLetters(alphabet[i], input);
-        prob[i] = (float)numOcurrences[i] / sizeof(input);
+        freq[i] = qtdLetters(alphabet[i], input);
+        prob[i] = (float)freq[i] / sizeof(input);
     }
 
-    print(numOcurrences, tam);
+    print(freq, tam);
     print(prob, tam);
 
-    sortByProb(prob, alphabet, tam);
+    // sortByProb(prob, alphabet, tam);
 
-    print(prob, tam);
-    printAlphabet(alphabet);
-    
+    // print(prob, tam);
+    // printAlphabet(alphabet);
 
-    // while (alphabet.size() > 1){
-    //     float s0 = getMenorProbabilidade(prob);
-    //     float s1 = getMenorProbabilidade(prob);
-    //     nodeArvore x;
-    //     x.esquerda = s0;
-    // }
+    int size = alphabet.size() / sizeof(alphabet[0]);
 
+    HuffmanCodes(alphabet, freq, size);
 
     //file.close();
     return 0;
@@ -155,10 +231,10 @@ void sortByProb(vector<float> &a, vector<char> &alph, int tam){
     }
 }
 
-float getMenorProbabilidade(vector<float> &prob){
-    vector<float>::iterator it = prob.end();
-    float aux = prob[prob.size() - 1];
-    prob.pop_back();
+// float getMenorProbabilidade(vector<float> &prob){
+//     vector<float>::iterator it = prob.end();
+//     float aux = prob[prob.size() - 1];
+//     prob.pop_back();
 
-    return aux;
-}
+//     return aux;
+// }
